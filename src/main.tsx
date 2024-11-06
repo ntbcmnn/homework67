d1b33c6f6a9258d728a3f88e6abba6354d9a1907
Natalya Botsman <botsmannatashaa@gmail.com>
2024-11-07 01:14:07 +0600

    Wrapped App container into browser router and provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-toastify/dist/ReactToastify.css';
+import { Provider } from 'react-redux';
+import { store } from './app/store.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <ToastContainer/>
-      <App/>
+      <Provider store={store}>
+        <App/>
+      </Provider>
     </BrowserRouter>
   </StrictMode>,
 );
