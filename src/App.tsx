51985c16b9687ef3d39d609943bcf5b52009ce33
Natalya Botsman <botsmannatashaa@gmail.com>
2024-11-07 01:41:46 +0600

    Displayed Toolbar, Calculator & Password components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import './App.css';
+import Password from './Containers/Password/Password.tsx';
+import Calculator from './Containers/Calculator/Calculator.tsx';
+import { Route, Routes } from 'react-router-dom';
+import ToolBar from './Components/ToolBar/ToolBar.tsx';
+
 
 const App = () => (
   <>
-    <button>button</button>
+    <ToolBar/>
+    <div className="container m-5">
+      <Routes>
+        <Route path="/" element={<Password/>}/>
+        <Route path="/pin" element={<Password/>}/>
+        <Route path="/calculator" element={<Calculator/>}/>
+      </Routes>
+    </div>
   </>
+
 );
 
 export default App;
